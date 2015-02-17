7b99c52499ae0450e3d0653356a0d0d9a766bdc8
omniaura <omniaura5@gmail.com>
2015-02-16 23:09:08 -0800

    frontend controller setup for editor

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,29 @@
 var app = angular.module('JsAnalyzer', ['ui.ace']);
 
-app.controller('EditorController', ['$scope', function($scope) {
-    $scope.greeting = 'Hola!';
+app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
+    $scope.editorText = "";
+
+    $scope.aceLoaded = function(_editor) {
+        _editor.setFontSize(16);
+    };
+
+    $scope.evaluate = function() {
+        if (this.timeoutId)
+            window.clearTimeout(this.timeoutId);
+        this.timeoutId = window.setTimeout(function(){
+            $http.post('/api/eval/', {editorText: $scope.editorText})
+                .success(function(data){
+                    console.log(data);
+            });
+        }, 1000);
+    }
+
+    $scope.$watch(
+        "editorText",
+        function(newValue, oldValue) {
+            if (newValue !== oldValue) {
+                $scope.evaluate();
+            }
+        }
+    )
 }]);
