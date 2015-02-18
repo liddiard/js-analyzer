3389a488003e24efdfa1dc90e3a17d911e7fef09
omniaura <omniaura5@gmail.com>
2015-02-17 18:09:03 -0800

    UI for program structure creator

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,8 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
             $http.post('/api/eval/', {editorText: $scope.editorText})
                 .success(function(data){
                     console.log(data);
-            });
+                }
+            );
         }, 1000);
     }
 
@@ -36,13 +37,39 @@ app.controller('ChallengeController', ['$scope', '$http', function($scope, $http
 
     $scope.whitelist = [];
     $scope.blacklist = [];
+    $scope.structure = [];
 
     $scope.notInBlacklist = function(item) {
         return ($scope.blacklist.indexOf(item.type) === -1);
     };
-    
+
     $scope.notInWhitelist = function(item) {
         return ($scope.whitelist.indexOf(item.type) === -1);
+    };
+
+    $scope.pushStructure = function() {
+        /* $scope.structure is an array of "alternative" arrays, which are composed
+         * of statement objects. We add a new empty alternative array here. */
+        $scope.structure.push([{}]);
+    };
+
+    $scope.pushNode = function(index) {
+        $scope.structure[index].push({});
+    };
+
+    $scope.removeNode = function(alternative, index, outerIndex) {
+        alternative.splice(index, 1);
+        console.log(outerIndex);
+        if (!alternative.length)
+            $scope.structure.splice(outerIndex, 1); // if the alternative is empty, remove it
+    };
+
+    $scope.addChild = function(node) {
+        node.child = {};
+    };
+
+    $scope.removeChild = function(node) {
+        delete node.child;
     };
 
 }]);
