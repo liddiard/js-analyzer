c10bc4cb96f1bd6e15a7b4a7c202ed20aad8f2c4
omniaura <omniaura5@gmail.com>
2015-02-19 01:13:16 -0800

    added comments, minor code structure improvements

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,39 @@
 var app = angular.module('JsAnalyzer', ['ui.ace']);
 
 app.controller('ChallengeController', ['$scope', function($scope) {
-    $scope.whitelist = [];
-    $scope.blacklist = [];
-    $scope.structure = [];
+    $scope.whitelist = []; // code MUST include these kinds of nodes
+    $scope.blacklist = []; // code MUST NOT include these kinds of nodes
+    $scope.structure = []; // code should be structured like this
+    $scope.nodeObjects = []; // available types of node objects
+
+    $http.get('/js/nodeObjects.json').success(function(data) {
+        $scope.nodeObjects = data;
+    });
 }]);
 
 app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
-    $scope.editorText = "";
+    $scope.editorText = ""; // user-input text
+    $scope.evaluating = false; // has the user entered text that has NOT been evaluated?
 
     $scope.aceLoaded = function(_editor) {
         _editor.setFontSize(16);
     };
 
     $scope.evaluate = function() {
-        var payload = {
-            editorText: $scope.editorText,
-            whitelist: $scope.whitelist,
-            blacklist: $scope.blacklist,
-            structure: $scope.structure
-        };
+        $scope.evaluating = true; // show "evaluating" indicator
         if (this.timeoutId)
             window.clearTimeout(this.timeoutId);
         this.timeoutId = window.setTimeout(function(){
+            var payload = {
+                editorText: $scope.editorText,
+                whitelist: $scope.whitelist,
+                blacklist: $scope.blacklist,
+                structure: $scope.structure
+            };
             $http.post('/api/eval/', payload)
                 .success(function(data){
                     console.log(data);
+                    $scope.evaluating = false; // hide "evaluating" indicator
                 }
             );
         }, 1000);
@@ -42,10 +50,6 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
 }]);
 
 app.controller('TestingController', ['$scope', '$http', function($scope, $http) {
-    $scope.nodeObjects = [];
-    $http.get('/js/nodeObjects.json').success(function(data) {
-        $scope.nodeObjects = data;
-    });
 
     $scope.notInBlacklist = function(item) {
         return ($scope.blacklist.indexOf(item.type) === -1);
@@ -62,10 +66,12 @@ app.controller('TestingController', ['$scope', '$http', function($scope, $http) 
     };
 
     $scope.pushNode = function(index) {
+        // add a node to the end of the structure
         $scope.structure[index].push({});
     };
 
     $scope.removeNode = function(alternative, index, outerIndex) {
+        // remove a node from the structure at a specified index
         alternative.splice(index, 1);
         if (!alternative.length)
             $scope.structure.splice(outerIndex, 1); // if the alternative is empty, remove it
